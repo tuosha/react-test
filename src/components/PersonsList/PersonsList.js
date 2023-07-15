fe7ee2d2273f92238686c278664ba646a62c98db
tuosha <tuosha@yandex.ru>
2023-07-15 18:37:44 +0300

    some fixes

--- src/components/PersonsList/PersonsList.js
+++ src/components/PersonsList/PersonsList.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react'
+import { getAllPersons } from '../../data/getDataApi'
+
 const PersonsList = () => {
-	
+	const [persons, setPersons] = useState([])
+	useEffect(() => {
+		getAllPersons().then(res => {
+			setPersons(res)
+		})
+	}, [])
+	return (
+	  persons.length && (
+			<ul className={'personal-list'}>
+				{persons.map(person =>
+				  <li key={person.id}>{person.name}</li>
+				)}
+			</ul>
+	  )
+	)
 }
 
 export default PersonsList
