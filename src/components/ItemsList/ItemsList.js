8da0202c8feae6be8d5cf78d80df765dc456adad
tuosha <tuosha@yandex.ru>
2023-07-28 11:43:32 +0300

    add delete record btn add custom Hook - useModel for 2-way binding

--- src/components/ItemsList/ItemsList.js
+++ src/components/ItemsList/ItemsList.js
@@ -1,15 +1,20 @@
 import './ItemsList.css'
 import { Item } from '../Item/Item'
+import { useState } from 'react'
 
 const ItemsList = ( {data} ) => {
+	const [currentData, setData] = useState(data)
+	const handleDelete = (id) => setData(currentData.filter(item => item.id !== id))
 	return (
-	  (data.length) && (
-		<ul className={'items-list'}>
-			{data.map(item =>
-			  <Item item={item} key={item.id}/>
-			)}
-		</ul>
-	  )
+	  (!currentData.length) ?
+	    'Empty list!' :
+		  (currentData.length) && (
+			<ul className={'items-list'}>
+				{currentData.map(item =>
+				  <Item item={item} key={item.id} handleDelete={handleDelete}/>
+				)}
+			</ul>
+		  )
 	)
 }
 
