11ee5cf4d5e82cc6ede698d04a8e8ff3822f18d9
tuosha <tuosha@yandex.ru>
2023-08-24 15:23:22 +0300

    add nanoid, fix unick keys problem, add checkInnerRecords utils, add testData from file

---
+++ src/data/testDataFromFile.js
@@ -0,0 +1,10 @@
+export const testData = [
+	{
+		id: 1,
+		name: [['hoh', ['bog', 'tog']], 'mogo']
+	},
+	{
+		id: 2,
+		name: 'oho'
+	}
+]
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,22 @@
+import { nanoid } from 'nanoid'
+
 const compose = (...fns) =>
 	fns.reduceRight((prevFn, nextFn) =>
 		(...args) => nextFn(prevFn(...args)), value => value
 );
 
+const checkInnerRecords = (records) => {
+	const checkNonEmptyObject = (typeof records === 'object' && records.length)
+	return checkNonEmptyObject ? (
+		<ul>{records.map(el =>
+		  <li key={nanoid()}>{
+			  checkNonEmptyObject ? checkInnerRecords(el) : el}
+		  </li>)}
+		</ul>) :
+	  records
+}
+
 export {
-	compose
+	compose,
+	checkInnerRecords
 }
